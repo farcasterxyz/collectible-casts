b0fa79856d93eb9a3364b50f22f947b257f153b0
horsefacts <webmaster@terminally.online>
2025-07-08 07:18:31 -0700

    feat: add auction parameter storage

--- src/Auction.sol
+++ src/Auction.sol
@@ -2,16 +2,28 @@
 pragma solidity ^0.8.30;
 
 import {IAuction} from "./interfaces/IAuction.sol";
+import {Ownable2Step} from "openzeppelin-contracts/contracts/access/Ownable2Step.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Auction is IAuction {
+contract Auction is IAuction, Ownable2Step {
     error InvalidAddress();
+
+    struct AuctionParams {
+        uint256 minBid;
+        uint256 minBidIncrement; // in basis points (10000 = 100%)
+        uint256 duration;
+        uint256 antiSnipeExtension;
+        uint256 antiSnipeThreshold;
+    }
 
     address public immutable collectibleCast;
     address public immutable minter;
     address public immutable usdc;
     address public immutable treasury;
 
-    constructor(address _collectibleCast, address _minter, address _usdc, address _treasury) {
+    AuctionParams private _defaultParams;
+
+    constructor(address _collectibleCast, address _minter, address _usdc, address _treasury) Ownable(msg.sender) {
         if (_collectibleCast == address(0)) revert InvalidAddress();
         if (_minter == address(0)) revert InvalidAddress();
         if (_usdc == address(0)) revert InvalidAddress();
@@ -21,5 +33,22 @@ contract Auction is IAuction {
         minter = _minter;
         usdc = _usdc;
         treasury = _treasury;
+
+        // Set default parameters
+        _defaultParams = AuctionParams({
+            minBid: 1e6, // 1 USDC (6 decimals)
+            minBidIncrement: 1000, // 10% in basis points
+            duration: 24 hours,
+            antiSnipeExtension: 15 minutes,
+            antiSnipeThreshold: 15 minutes
+        });
+    }
+
+    function setDefaultParams(AuctionParams memory params) external onlyOwner {
+        _defaultParams = params;
+    }
+
+    function defaultParams() external view returns (AuctionParams memory) {
+        return _defaultParams;
     }
 }
--- test/Auction/Auction.t.sol
+++ test/Auction/Auction.t.sol
@@ -5,6 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {Auction} from "../../src/Auction.sol";
 import {ICollectibleCast} from "../../src/interfaces/ICollectibleCast.sol";
 import {IMinter} from "../../src/interfaces/IMinter.sol";
+import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract AuctionTest is Test {
     Auction public auction;
@@ -43,5 +44,49 @@ contract AuctionTest is Test {
     function test_Constructor_RevertsIfTreasuryIsZero() public {
         vm.expectRevert(Auction.InvalidAddress.selector);
         new Auction(COLLECTIBLE_CAST, MINTER, USDC, address(0));
+    }
+
+    function test_SetDefaultParams_StoresCorrectly() public {
+        Auction.AuctionParams memory params = Auction.AuctionParams({
+            minBid: 1e6, // 1 USDC (6 decimals)
+            minBidIncrement: 1000, // 10% in basis points
+            duration: 24 hours,
+            antiSnipeExtension: 15 minutes,
+            antiSnipeThreshold: 15 minutes
+        });
+
+        vm.prank(auction.owner());
+        auction.setDefaultParams(params);
+
+        Auction.AuctionParams memory storedParams = auction.defaultParams();
+        assertEq(storedParams.minBid, 1e6);
+        assertEq(storedParams.minBidIncrement, 1000);
+        assertEq(storedParams.duration, 24 hours);
+        assertEq(storedParams.antiSnipeExtension, 15 minutes);
+        assertEq(storedParams.antiSnipeThreshold, 15 minutes);
+    }
+
+    function test_SetDefaultParams_OnlyOwner() public {
+        Auction.AuctionParams memory params = Auction.AuctionParams({
+            minBid: 1e6,
+            minBidIncrement: 1000,
+            duration: 24 hours,
+            antiSnipeExtension: 15 minutes,
+            antiSnipeThreshold: 15 minutes
+        });
+
+        address notOwner = address(0x123);
+        vm.prank(notOwner);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, notOwner));
+        auction.setDefaultParams(params);
+    }
+
+    function test_Constructor_SetsDefaultParams() public view {
+        Auction.AuctionParams memory params = auction.defaultParams();
+        assertEq(params.minBid, 1e6); // 1 USDC
+        assertEq(params.minBidIncrement, 1000); // 10%
+        assertEq(params.duration, 24 hours);
+        assertEq(params.antiSnipeExtension, 15 minutes);
+        assertEq(params.antiSnipeThreshold, 15 minutes);
     }
 }
